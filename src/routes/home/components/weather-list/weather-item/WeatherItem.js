36d83f604732ca1722c496b5b6310dd67e27a903
kit <nickmat95@yandex.ru>
2017-08-21 19:59:06 +0300

    edits in styles

--- src/routes/home/components/weather-list/weather-item/WeatherItem.js
+++ src/routes/home/components/weather-list/weather-item/WeatherItem.js
@@ -9,22 +9,22 @@ class WeatherItem extends React.Component {
   render() {
     let link = `/detailed/${this.props.townID}`;
      return(
-       <div className={s.weatherItem}>
-         <div className={s.weatherItem__iconWrap}>
-            <img className={s.weatherItem__icon} src={this.props.img} />
-         </div>
-         <p className={s.weatherItem__town}>
-            <Link className={s.weatherItem__link} to={link}>{this.props.town}</Link>
-         </p>
-         <p className={s.weatherItem__dayTemp} title="day temperature">{this.props.temperatureDay}</p>
-         <p className={s.weatherItem__nightTemp} title="night temperature">{this.props.temperatureNight}</p>
-         <p className={s.weatherItem__weatherIndicator}><span className={s.weatherItem__weatherIndicatorTitle}>temperature water:</span> {this.props.temperatureWater}</p>
-         <p className={s.weatherItem__weatherIndicator}><span className={s.weatherItem__weatherIndicatorTitle}>cloudiness:</span> {this.props.cloudiness}</p>
-         <p className={s.weatherItem__weatherIndicator}><span className={s.weatherItem__weatherIndicatorTitle}>precipitation:</span> {this.props.precipitation}</p>
-         <p className={s.weatherItem__weatherIndicator}><span className={s.weatherItem__weatherIndicatorTitle}>pressure:</span> {this.props.pressure} mmHg</p>
-         <p className={s.weatherItem__weatherIndicator}><span className={s.weatherItem__weatherIndicatorTitle}>humidity:</span> {this.props.humidity}%</p>
-         <p className={s.weatherItem__weatherIndicator}><span className={s.weatherItem__weatherIndicatorTitle}>wind speed:</span> {this.props.windSpeed} m/s</p>
-       </div>
+        <Link className={s.weatherItem} to={link}>
+            <div className={s.weatherItem__iconWrap}>
+                <img className={s.weatherItem__icon} src={this.props.img} />
+            </div>
+            <p className={s.weatherItem__town}>
+                {this.props.town}
+            </p>
+            <p className={s.weatherItem__dayTemp} title="day temperature">{this.props.temperatureDay}</p>
+            <p className={s.weatherItem__nightTemp} title="night temperature">{this.props.temperatureNight}</p>
+            <p className={s.weatherItem__weatherIndicator}><span className={s.weatherItem__weatherIndicatorTitle}>temperature water:</span> {this.props.temperatureWater}</p>
+            <p className={s.weatherItem__weatherIndicator}><span className={s.weatherItem__weatherIndicatorTitle}>cloudiness:</span> {this.props.cloudiness}</p>
+            <p className={s.weatherItem__weatherIndicator}><span className={s.weatherItem__weatherIndicatorTitle}>precipitation:</span> {this.props.precipitation}</p>
+            <p className={s.weatherItem__weatherIndicator}><span className={s.weatherItem__weatherIndicatorTitle}>pressure:</span> {this.props.pressure} mmHg</p>
+            <p className={s.weatherItem__weatherIndicator}><span className={s.weatherItem__weatherIndicatorTitle}>humidity:</span> {this.props.humidity}%</p>
+            <p className={s.weatherItem__weatherIndicator}><span className={s.weatherItem__weatherIndicatorTitle}>wind speed:</span> {this.props.windSpeed} m/s</p>
+        </Link>
      );
    }
 }
